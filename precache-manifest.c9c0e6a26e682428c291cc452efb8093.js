3ee19f4f339a3d7206c28dae1937c5e647380fe2
Ignat <decl011@tut.by>
2020-05-06 21:12:52 +0300

    Updates

---
+++ precache-manifest.c9c0e6a26e682428c291cc452efb8093.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "896fddf0032949f971e1",
+    "url": "/MafiaShop/static/css/main.37a5da29.chunk.css"
+  },
+  {
+    "revision": "896fddf0032949f971e1",
+    "url": "/MafiaShop/static/js/main.896fddf0.chunk.js"
+  },
+  {
+    "revision": "b5b2c463dd3054f36aa3",
+    "url": "/MafiaShop/static/js/1.b5b2c463.chunk.js"
+  },
+  {
+    "revision": "e6e84314c6eb01b8e2fa",
+    "url": "/MafiaShop/static/js/runtime~main.e6e84314.js"
+  },
+  {
+    "revision": "78df4b6085311de8072e8980cc3599d6",
+    "url": "/MafiaShop/index.html"
+  }
+];
